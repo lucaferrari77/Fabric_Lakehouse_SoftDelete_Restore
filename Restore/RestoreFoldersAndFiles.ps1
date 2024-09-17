0f356f94fdb39f43c67f95e47fb1b6654e1de644
lucaferrari77 <90248940+lucaferrari77@users.noreply.github.com>
2024-09-17 14:22:58 +0200

    Update RestoreFoldersAndFiles.ps1

--- Restore/RestoreFoldersAndFiles.ps1
+++ Restore/RestoreFoldersAndFiles.ps1
@@ -1,1 +1,21 @@
+#Installing the Az.Storage module
+Install-Module Az.Storage -Repository PSGallery -Force
+Import-Module Az.Storage
 
+#connecting to Azure
+Connect-AzAccount
+
+#changing the context
+Get-AzContext -ListAvailable | Select-Object -Property *
+
+Settinmg the context using the proper subscription id
+Set-AzContext -SubscriptionId 'Your SubscritionId'
+
+#Creating the storage context to be used during the discovery and the restore
+$ctx = New-AzStorageContext -StorageAccountName 'onelake' -UseConnectedAccount -endpoint 'fabric.microsoft.com' 
+
+#Getting the list: Deleted files
+Get-AzDataLakeGen2DeletedItem -Context $ctx -FileSystem 'your workspace' -Path 'yourlakehouse.lakehouse' | Select Path, DeletionId
+
+#Restoring the file
+Restore-AzDataLakeGen2DeletedItem -Context $ctx -FileSystem 'your workspace'  -Path 'deleteitempath' -DeletionId 133709673683448292
